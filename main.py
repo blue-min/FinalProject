019ddc5e2d1f60fe86959178af70670be82f175d
Teemo0708 <jms3800@gmail.com>
2022-08-29 20:18:09 +0900

    랩탑 연동 테스트

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('미리~1')
+    print_hi('명호')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
