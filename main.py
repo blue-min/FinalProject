82a236b7c04532f0d9422348042c5ecaf51a1987
miryS2 <alfl8813@naver.com>
2022-08-29 12:09:48 +0900

    안녕

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('정민~')
+    print_hi('미리~')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
